c209eed7b8b4aef6055d9e4756dda7ed8a7785b3
ShrayHub <shravyavermav@gmail.com>
2025-04-24 17:29:14 +0530

    Initial Commit

---
+++ bookmarks.js
@@ -0,0 +1,25 @@
+// bookmarks.js
+
+function getBookmarks() {
+    return JSON.parse(localStorage.getItem('airQualityBookmarks')) || [];
+}
+
+function saveBookmarks(bookmarks) {
+    localStorage.setItem('airQualityBookmarks', JSON.stringify(bookmarks));
+}
+
+function addBookmark(newBookmark) {
+    const bookmarks = getBookmarks();
+    bookmarks.push(newBookmark);
+    saveBookmarks(bookmarks);
+}
+
+function removeStoredBookmark(index) {
+    const bookmarks = getBookmarks();
+    bookmarks.splice(index, 1);
+    saveBookmarks(bookmarks);
+}
+
+function clearAllStoredBookmarks() {
+    localStorage.removeItem('airQualityBookmarks');
+}
