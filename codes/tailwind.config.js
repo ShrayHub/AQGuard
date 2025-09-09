16ff4e2be1cdbbe185dd9ae6977b840ea6ca941e
Shravya Verma <shravyavermav@gmail.com>
2025-09-09 16:04:29 +0530

    Add files via upload

---
+++ codes/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    darkMode: 'class', // Enables class-based dark mode toggling
+    theme: {
+      extend: {
+        // You can customize the colors, fonts, etc. here if needed
+      },
+    },
+    variants: {
+      extend: {
+        backgroundColor: ['dark'],
+        textColor: ['dark'],
+      },
+    },
+    plugins: [],
+  }
+  
